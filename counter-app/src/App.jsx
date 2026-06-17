29c7ce4ba36075d61df1781fedc443ed14d44829
shashank <shashankchamoli069@gmail.com>
2026-06-17 20:34:30 +0530

    React learning Day -2

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+}
+export default App;
--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,20 +1,38 @@
 import "./Counter.css";
-import { useState }  from "react";
+import { useState } from "react";
 
 function Counter() {
-  const[count, setCount] = useState(0);
-  
-  const increment = ()=>{
-    setCount(count+1);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
   };
 
-  const decrement = ()=>{
-    setCount(count-1);
+  const decrement = () => {
+    setCount(count - 1);
   };
 
-  const reset =() =>{
+  const reset = () => {
     setCount(0);
   };
 
-  return();
+  return (
+    <div className="counter-container">
+      <h1>Button Counter</h1>
+      <h2>{count}</h2>
+      <div className="buttons">
+        <button className="minus" onClick={decrement}>
+          -
+        </button>
+        <button className="reset" onClick={reset}>
+          RESET
+        </button>
+        <button className="plus" onClick={increment}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
+
+export default Counter;
